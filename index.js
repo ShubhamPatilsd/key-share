b3fb1bedcccc1c95272ba237704497752a334b7e
ShubhamPatilsd <55328098+ShubhamPatilsd@users.noreply.github.com>
2021-02-15 15:21:59 -0800

    key share! i've been working on it for the past few days. now imma try and get it on vercel

---
+++ index.js
@@ -0,0 +1,67 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBnTaZJ4mhNVrzZNuk6wiZwek23ypQDw_s",
+    authDomain: "keyshare-f0f98.firebaseapp.com",
+    databaseURL: "https://keyshare-f0f98-default-rtdb.firebaseio.com",
+    projectId: "keyshare-f0f98",
+    storageBucket: "keyshare-f0f98.appspot.com",
+    messagingSenderId: "364324106355",
+    appId: "1:364324106355:web:c15c6e586f56da911cfa06"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db=firebase.database();
+
+function showTheSwal(s,titleThing){
+    
+    Swal.fire({
+        title: titleThing,
+        showCloseButton: true,
+        //html: '<iframe width="100%" height="500px" src="https://www.youtube.com/embed/aeqnEJpPZVY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        html: '<iframe width="100%" height="720px" src="'+s+'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        customClass: 'swal-big',
+        showConfirmButton: false // There won't be any confirm button
+    });
+}
+
+
+const fetchChat=db.ref("sounds/");
+fetchChat.on("child_added",function(snapshot){
+    const message = snapshot.val();
+    let test="'"+message.embed+"','"+message.title+"'";
+    const msg = `<div class="responsive">
+    <div class="gallery" onclick="showTheSwal(`+test+`)">
+        
+            <img src="`+message.thumbnail+`" alt="Video Thumbail" width="1280" height="720">
+        
+        
+        <div class="desc">`+message.title+`</div>
+    </div>
+    </div>`;
+
+    document.getElementById("keebs").innerHTML += msg;
+    
+})
+
+
+/*      <div class="responsive">
+            <div class="gallery" onclick="showTheSwal('https\://www.youtube.com/embed/hMoTeGyCAtc?start=513')">
+                
+                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F7c7_FDBXA1o%2Fmaxresdefault.jpg&f=1&nofb=1" alt="Cinque Terre" width="600" height="400">
+                
+                
+                <div class="desc">Add a description of the image here</div>
+            </div>
+            </div>
+
+            <div class="responsive">
+                <div class="gallery">
+                    <a target="_blank" href="#">
+                        <img src="https://i.ytimg.com/vi/FjTrVZAfUcU/maxresdefault.jpg" alt="Cinque Terre" width="600" height="400">
+                      </a>
+                    
+                    <div class="desc">Add a description of the image here</div>
+                </div>
+                </div>
+                */
+
---
+++ upload.js
@@ -0,0 +1,96 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBnTaZJ4mhNVrzZNuk6wiZwek23ypQDw_s",
+    authDomain: "keyshare-f0f98.firebaseapp.com",
+    databaseURL: "https://keyshare-f0f98-default-rtdb.firebaseio.com",
+    projectId: "keyshare-f0f98",
+    storageBucket: "keyshare-f0f98.appspot.com",
+    messagingSenderId: "364324106355",
+    appId: "1:364324106355:web:c15c6e586f56da911cfa06"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db=firebase.database();
+
+/*document.getElementById("form").addEventListener("submit",send);
+
+function send(e){      
+      //<iframe width="100%" height="500px" src="https://www.youtube.com/embed/Za0qIOprR1Q?start=391" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      let timestamp=document.getElementById("timestamp").value.split(":");
+      let secondsCounter=0;
+      let multiplier=1;
+      console.log(timestamp);
+      for(let i=timestamp.length-1;i>-1;i--){
+        secondsCounter+=timestamp[i]*multiplier;
+        multiplier*=60;
+      }
+      
+      db.ref('/messages/' + (Date.now()).toString()).set({
+        embed: "https://www.youtube.com/embed/"+document.getElementById("url").value.slice(32)+"?start="+secondsCounter,
+        time: Date.now()
+      });
+      
+    
+      console.log("Sent");
+    
+    
+}*/
+document.getElementById("form").addEventListener("submit",postChat);
+
+
+
+
+
+function postChat(e){
+    e.preventDefault();
+    
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to delete this posting!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, post it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Sent!',
+          'The video and timestamp are posting. You will be redirected to the homepage soon',
+          'success'
+        )
+
+        let timestamp=document.getElementById("timestamp").value.split(":");
+    
+    
+    let secondsCounter=0;
+    let multiplier=1;
+      
+      for(let i=timestamp.length-1;i>-1;i--){
+        secondsCounter+=timestamp[i]*multiplier;
+        multiplier*=60;
+      }
+    
+    
+    
+    db.ref("sounds/"+Date.now()).set({
+        embed: "https://www.youtube.com/embed/"+document.getElementById("url").value.slice(32)+"?start="+secondsCounter,
+        thumbnail: "http://img.youtube.com/vi/"+document.getElementById("url").value.slice(32)+"/maxresdefault.jpg",
+        title: document.getElementById("title").value
+        
+    });
+    
+    setTimeout(function(){
+      window.location.href="index.html";
+    },1200)
+        //window.location.href="index.html";
+      }
+    })
+
+
+
+    
+    
+    
+}
+
